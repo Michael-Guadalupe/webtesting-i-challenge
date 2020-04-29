8c1e183c17da750ad9ca2f59bf5742052225c2da
Jackie Atwood <takalaj@verizon.net>
2020-04-28 22:39:54 -0400

    Functions Scaffolded

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -18,11 +18,32 @@ function succeed(item) {
 }
 
 function fail(item) {
-  return { ...item };
+  let durability = item.durability;
+  let enhancement = item.enhancement;
+
+  if (enhancement < 15) {
+    durability -= 5;
+  } else {
+   durability -= 10;
+  }
+
+  if (enhancement > 16) {
+    enhancement -= 1;
+  }
+  return { ...item, enhancement: enhancement, durability: durability };
+  // return { ...item, enhancement, durability };
 }
 
+
+
 function repair(item) {
-  return { ...item };
+  const durability = item.durability;
+
+  if (durability < 100) {
+    return {...item, durability: 100}
+  } else {
+    return { ...item };
+  }
 }
 
 function get(item) {
